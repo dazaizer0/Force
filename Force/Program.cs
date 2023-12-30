eee56c3001b46d0e9b2f5a190ad264fe0bf74329
dazai <60112867+dazaizer0@users.noreply.github.com>
2023-12-30 21:08:56 +0100

    SceneManagementFix

--- Force/Program.cs
+++ Force/Program.cs
@@ -1,9 +1,17 @@
 using Force.Engine.ForceAll.StaticManager;
+using System;
 
 // PROPERTIES
-using var scene1 = new Force.Game.Examples.Platformer();
 SceneManager.ActualScene = 0;
 
 // PROGRAM
 if (SceneManager.ActualScene == 0)
+{
+    using var scene1 = new Force.Game.Examples.Platformer();
     scene1.Run();
+}
+else if (SceneManager.ActualScene == 1)
+{
+    using var scene1 = new Force.Game.Examples.Physics();
+    scene1.Run();
+}
